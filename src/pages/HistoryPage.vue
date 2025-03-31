27fce8e93e1d28e1e6ee22ac7e5b797e9a6ea037
FilipHires316 <xhires@stuba.sk>
2025-04-01 00:29:59 +0200

    split screen

--- src/pages/HistoryPage.vue
+++ src/pages/HistoryPage.vue
@@ -9,7 +9,7 @@
     @click="toggleLeftDrawer"
     style="position: fixed; z-index: 9998; top: 100px; left: 0"
   />
-  <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+  <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered overlay>
     <q-scroll-area style="height: 100%; flex: 1" class="custom-scroll">
       <div class="Menu">
         <q-expansion-item
@@ -27,35 +27,112 @@
             class="menuButton"
             :label="item.algorithm + ' + ' + item.problem"
             stack
-            @click="solutionIndexSet(index)"
+            @click="leftSolutionIndexSet(index)"
           />
         </q-expansion-item>
       </div>
     </q-scroll-area>
   </q-drawer>
 
-  <q-page class="column items-center justify-evenly">
-    <div>
-      {{entries[solutionIndex]}}
+  <q-btn
+    v-if="(!isMobile || !rightDrawerOpen) && !screenSplit"
+    dense
+    flat
+    round
+    color="black"
+    icon="menu"
+    @click="toggleRightDrawer"
+    style="position: fixed; z-index: 9998; top: 100px; right: 0"
+  />
+
+  <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered overlay v-show="!screenSplit">
+    <q-scroll-area style="height: 100%; flex: 1" class="custom-scroll">
+      <div class="Menu">
+        <q-expansion-item
+          class="MenuBanner"
+          expand-separator
+          label="HISTÓRIA"
+          default-opened
+          header-style="border-radius: 20px"
+          expand-icon-class="text-white"
+          header-class="bg-primary"
+        >
+          <q-btn
+            v-for="(item, index) in history.entries"
+            :key="index"
+            class="menuButton"
+            :label="item.algorithm + ' + ' + item.problem"
+            stack
+            @click="rightSolutionIndexSet(index)"
+          />
+        </q-expansion-item>
+      </div>
+    </q-scroll-area>
+  </q-drawer>
+
+  <div class="page-container">
+    <div style="padding-left: 5vw; padding-right: 5vw;">
+      <q-page class="column items-center justify-evenly page">
+        <div>
+          {{ entries[leftSolutionIndex] }}
+        </div>
+        <div v-if="entries.length > 0" class="button-container" style="margin-top: 10px">
+          <q-btn
+            class="button"
+            color="white"
+            text-color="black"
+            label="Predchádzajúce"
+            @click="leftSolutionIndexDecrement"
+            style="width: 10vw"
+          />
+          <q-btn
+            class="button"
+            color="primary"
+            label="Ďalšie"
+            @click="leftSolutionIndexIncrement"
+            style="width: 10vw"
+          />
+        </div>
+        <q-btn
+          color="grey"
+          :icon="screenSplit ? 'add' : 'remove'"
+          @click="screenSplitShift"
+          style="width: 4vw; height: 4vw; position: fixed; bottom: 1vw; right: 1vw;"
+        />
+      </q-page>
     </div>
-    <div v-if="entries.length > 0" class="button-container" style="margin-top: 10px">
-      <q-btn
-        class="button"
-        color="white"
-        text-color="black"
-        label="Predchádzajúce"
-        @click="solutionIndexDecrement"
-        style="width: 10vw"
-      />
-      <q-btn
-        class="button"
-        color="primary"
-        label="Ďalšie"
-        @click="solutionIndexIncrement"
-        style="width: 10vw"
-      />
+
+    <div style="padding-left: 5vw; padding-right: 5vw;" v-show="!screenSplit">
+      <q-page class="column items-center justify-evenly page">
+        <div>
+          {{ entries[rightSolutionIndex] }}
+        </div>
+        <div v-if="entries.length > 0" class="button-container" style="margin-top: 10px">
+          <q-btn
+            class="button"
+            color="white"
+            text-color="black"
+            label="Predchádzajúce"
+            @click="rightSolutionIndexDecrement"
+            style="width: 10vw"
+          />
+          <q-btn
+            class="button"
+            color="primary"
+            label="Ďalšie"
+            @click="rightSolutionIndexIncrement"
+            style="width: 10vw"
+          />
+        </div>
+        <q-btn
+          color="grey"
+          :icon="screenSplit ? 'add' : 'remove'"
+          @click="screenSplitShift"
+          style="width: 4vw; height: 4vw; position: fixed; bottom: 1vw; right: 1vw;"
+        />
+      </q-page>
     </div>
-  </q-page>
+  </div>
 </template>
 
 <script lang="ts">
@@ -73,26 +150,54 @@ export default defineComponent({
     const {
       entries
     } = storeToRefs(history);
+
     const leftDrawerOpen = ref(false)
     const toggleLeftDrawer = () => {
       leftDrawerOpen.value = !leftDrawerOpen.value
     }
 
-    const solutionIndex = ref(history.entries.length - 1)
-
-    const solutionIndexIncrement = () => {
-      solutionIndex.value++
-      if (solutionIndex.value > history.entries.length - 1)
-        solutionIndex.value = 0
+    const rightDrawerOpen = ref(false)
+    const toggleRightDrawer = () => {
+      rightDrawerOpen.value = !rightDrawerOpen.value
     }
 
-    const solutionIndexDecrement = () => {
-      solutionIndex.value--
-      if (solutionIndex.value < 0)
-        solutionIndex.value = history.entries.length - 1
+    const leftSolutionIndex = ref(history.entries.length - 1)
+    const rightSolutionIndex = ref(history.entries.length - 1)
+
+    const screenSplit = ref(true)
+
+    const screenSplitShift = () => {
+      screenSplit.value = !screenSplit.value
     }
-    const solutionIndexSet = (index: number) => {
-      solutionIndex.value = index
+
+    const leftSolutionIndexIncrement = () => {
+      leftSolutionIndex.value++
+      if (leftSolutionIndex.value > history.entries.length - 1)
+        leftSolutionIndex.value = 0
+    }
+
+    const leftSolutionIndexDecrement = () => {
+      leftSolutionIndex.value--
+      if (leftSolutionIndex.value < 0)
+        leftSolutionIndex.value = history.entries.length - 1
+    }
+    const leftSolutionIndexSet = (index: number) => {
+      leftSolutionIndex.value = index
+    }
+
+    const rightSolutionIndexIncrement = () => {
+      rightSolutionIndex.value++
+      if (rightSolutionIndex.value > history.entries.length - 1)
+        rightSolutionIndex.value = 0
+    }
+
+    const rightSolutionIndexDecrement = () => {
+      rightSolutionIndex.value--
+      if (rightSolutionIndex.value < 0)
+        rightSolutionIndex.value = history.entries.length - 1
+    }
+    const rightSolutionIndexSet = (index: number) => {
+      rightSolutionIndex.value = index
     }
 
     const isMobile = ref(false)
@@ -114,12 +219,20 @@ export default defineComponent({
       history,
       toggleLeftDrawer,
       leftDrawerOpen,
-      solutionIndex,
+      toggleRightDrawer,
+      rightDrawerOpen,
+      leftSolutionIndex,
+      rightSolutionIndex,
       isMobile,
-      solutionIndexIncrement,
-      solutionIndexDecrement,
-      solutionIndexSet,
-      entries
+      leftSolutionIndexIncrement,
+      leftSolutionIndexDecrement,
+      leftSolutionIndexSet,
+      rightSolutionIndexIncrement,
+      rightSolutionIndexDecrement,
+      rightSolutionIndexSet,
+      entries,
+      screenSplit,
+      screenSplitShift
     }
   },
 })
@@ -165,5 +278,11 @@ export default defineComponent({
   justify-content: center;
   gap: 1vw;
   margin-bottom: 2vw;
+}
+
+.page-container {
+  display: flex;
+  width: 100vw;
+  height: 100vh;
 }
 </style>
