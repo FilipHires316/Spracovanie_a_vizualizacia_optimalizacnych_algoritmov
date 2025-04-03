408b6179416ef99cbb79172f865f61f0ef38f922
FilipHires316 <xhires@stuba.sk>
2025-04-03 20:44:23 +0200

    ende of all problems

--- src/stores/problems/salesmanProblem.ts
+++ src/stores/problems/salesmanProblem.ts
@@ -6,13 +6,40 @@ export const useSalesmanProblem = defineStore('salesmanProblem', () => {
 
   const paramStore = useParamStore();
 
+  const rearrange = (solution: number[]): number[] => {
+    let missingNumbers = Array.from({ length: paramStore.cities.length }, (_, index) => index);
+    missingNumbers = missingNumbers.filter(num => !solution.includes(num));
+    for (let i = missingNumbers.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const valueI = missingNumbers[i] ?? 0;
+      const valueJ = missingNumbers[j] ?? 0;
+      [missingNumbers[i], missingNumbers[j]] = [valueJ, valueI];
+    }
+    const seen = new Set<number>();
+    for (let i = 0; i < solution.length; i++) {
+      const x = solution[i];
+      if (x) {
+        if (seen.has(x) && missingNumbers.length > 0) {
+          solution[i] = missingNumbers.shift()!;
+        }
+        seen.add(x);
+      }
+    }
+    return solution;
+  };
+
   const createSolutions = (): number[][] => {
     const solutions: number[][] = [];
     let counter: number = 0;
-
     if (paramStore.population) {
       while (solutions.length < paramStore.population) {
-        const currentSolution: number[] = Array.from({ length: paramStore.cities.length + 1 }, (_, index) => index);
+        const currentSolution: number[] = Array.from({ length: paramStore.cities.length }, (_, index) => index);
+        for (let i = currentSolution.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          const valueI = currentSolution[i] ?? 0;
+          const valueJ = currentSolution[j] ?? 0;
+          [currentSolution[i], currentSolution[j]] = [valueJ, valueI];
+        }
         if (!solutions.some(sol => sol.every((val, index) => val === currentSolution[index]))) {
           solutions.push(currentSolution);
           counter = 0;
@@ -27,18 +54,42 @@ export const useSalesmanProblem = defineStore('salesmanProblem', () => {
     return solutions;
   };
 
-
-  const calculateFitness = () => {
-    return
-  };
+  const calculateFitness = (solution: number[], iteration: number) => {
+    console.log(iteration)
+    let fitness = 0
+    let previousCity = paramStore.start[0]
+    for (let i = 0; i < solution.length; i++) {
+      const currentCity = paramStore.cities[i]
+      if (currentCity && previousCity) {
+        const deltaX = currentCity.x - previousCity.x;
+        const deltaY = currentCity.y - previousCity.y;
+        const distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+        fitness += distance
+        previousCity = paramStore.cities[i]
+      }
+    }
+    return fitness
+  }
 
   const getProblemType = () => {
     return 'salesman'
   };
 
   const mutate = (population: Chromosome[], mutationRate: number) => {
-    console.log(mutationRate)
-    return population
+    population.forEach(individual => {
+      individual.solution = rearrange(individual.solution);
+      for (let index = 0; index < individual.solution.length; index++) {
+        if (Math.random() * 100 < mutationRate) {
+          let j = index
+          while (index == j)
+          j = Math.floor(Math.random() * (index + 1));
+          const valueI = solution[index] ?? 0;
+          const valueJ = solution[j] ?? 0;
+          [solution[index], solution[j]] = [valueJ, valueI];
+        }
+      }
+    });
+    return population;
   };
 
   return {
